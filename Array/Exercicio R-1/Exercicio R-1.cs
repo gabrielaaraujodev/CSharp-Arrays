5d6a405ada49cfd3651933c89a98893442d89b38
Gabriel Araujo <aaraujoslgabriel@gmail.com>
2024-04-26 10:45:58 -0300

    Final exercises

--- Array/Exercicio R-1/Exercicio R-1.cs
+++ Array/Exercicio R-1/Exercicio R-1.cs
@@ -10,9 +10,28 @@ namespace Exercicio_R_1
     {
         static void Main(string[] args)
         {
+            int tamanhoArray = 100;
+            double[] vet = new double[tamanhoArray];
+            int cont = 0, cont2 = 0;
 
+            while (cont < tamanhoArray ) 
+            {
+                if (cont2 % 7 != 0 || cont2 % 10 == 7)
+                {
+                    vet[cont] = cont2;
+                    cont++;
+                }
 
+                cont2++;
+            }
 
+            foreach (int n in vet) 
+            {
+                Console.Write(n + " ");   
+            }
+            
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Array/Exercicio R-2/Exercicio R-2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_R_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanhoVetor = 10;
+            int[] vetA = new int[tamanhoVetor];
+            int[] vetB = new int[tamanhoVetor];
+            int[] vetC = new int[tamanhoVetor/2];
+
+            for (int i = 0; i < vetA.Length; i++)
+            {
+                Console.WriteLine($"Informe o {i+1}º do vetor A: ");
+                vetA[i] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Informe o {i + 1}º do vetor B: ");
+                vetB[i] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                if (vetA[i] == vetB[i])
+                {
+                    while (vetA[i] == vetB[i])
+                    {
+                        Console.WriteLine("Entre com um novo valor: ");
+                        vetA[i] = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (int n in vetA)
+            {
+                Console.Write(n + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach(int x in vetB)
+            {
+                Console.Write(x + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0;i < vetC.Length;i++)
+            {
+                vetC[i] = vetA[i] - vetB[i]; 
+            }
+
+            Console.WriteLine();
+
+            foreach (int z in vetC)
+            {
+                Console.Write(z + " ");
+            }
+
+           Console.ReadLine();
+        }
+    }
+}
